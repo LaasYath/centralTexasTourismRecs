97666105ba23386d0322024a997e3895ff45d54f
LaasYath <67027434+LaasYath@users.noreply.github.com>
2022-05-30 12:06:02 -0500

    Explore history is styled

--- homeFiles/homeFunctions.js
+++ homeFiles/homeFunctions.js
@@ -6,3 +6,35 @@ function hidePopup () {
         box.style.visibility = 'hidden';
     });
 }
+
+// function changeColor(item) {
+//     // Changes Text Color
+//     var title = document.getElementById("histTitle");
+//     title.style.color = "black";
+
+//     // Draws image to canvas (to be altered)
+//     var c = document.getElementById("histCanvas");
+//     var ctx = c.getContext('2d');
+//     var img = document.getElementById("histImg");
+//     ctx.drawImage(img, 0, 0, c.width, c.height);
+
+//     // pixel manipulation
+//     var pixData = ctx.getImageData(0, 0, c.width, c.height);
+//     var data = pixData.data;
+//     for (var i = 0; i < data.length; i+=4) {
+//         var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+//         data[i]     = avg; // red
+//         data[i + 1] = avg; // green
+//         data[i + 2] = avg; // blue
+//     }
+//     ctx.putImageData(pixData, 0, 0);
+// }
+
+function changeColor(item) {
+    item.style.color = "black";
+}
+
+
+function changeBack(item) {
+    item.style.color = "white";
+}
--- hotelFiles/hotel.php
+++ hotelFiles/hotel.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> CONTACT | Central Texas Tourism Reccomendations! </title>
-    <link rel="stylesheet" href="formStyle.css">
+    <link rel="stylesheet" href="formStyle.css?v=<?pho echo time() ?>">
 </head>
 <body>
     <!-- Meny Buttons (Link to Other Pages) -->
