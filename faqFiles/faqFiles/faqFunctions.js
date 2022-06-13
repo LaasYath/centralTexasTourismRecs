c17b290dd165dc4e8fe7d02b4bd239a20c604deb
LaasYath <67027434+LaasYath@users.noreply.github.com>
2022-06-13 15:17:29 -0500

    Answer View Control Fully Working

--- faqFiles/faqFiles/faqFunctions.js
+++ faqFiles/faqFiles/faqFunctions.js
@@ -9,6 +9,11 @@ function changeBgBack() {
 }
 
 function view(item) {
-    // var answer = document.getElementById(item);
-    item.style.display = "none";
+    var answer = document.getElementById(item);
+    let compStyles = window.getComputedStyle(answer);
+    if (compStyles.display == "none") {
+        answer.style.display = "block";
+    } else if (compStyles.display == "block") {
+        answer.style.display = "none";
+    }
 }
