6502580addced87ce08e3983d4e1a1d3ea01c491
LaasYath <67027434+LaasYath@users.noreply.github.com>
2022-06-13 15:01:36 -0500

    Update buttons

--- faqFiles/faqFiles/faqFunctions.js
+++ faqFiles/faqFiles/faqFunctions.js
@@ -1,3 +1,13 @@
+// Changes home logo when hovered
+function changeBg() {
+    var logo = document.getElementById("homeLogo");
+    logo.src="darkWebLogo.png";
+}
+function changeBgBack() {
+    var logo = document.getElementById("homeLogo");
+    logo.src="webLogo.png";
+}
+
 function view(item) {
     // var answer = document.getElementById(item);
     item.style.display = "none";
